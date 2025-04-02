ec5da9733aa25f328434a5d8d519fc68f12c6fc0
naphiale <nafia.levana19@gmail.com>
2025-04-03 01:33:00 +0700

    feat: add urls for transactions

--- funds/urls.py
+++ funds/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 
-from funds.views import CreateTransaction, GetBudgetRequestById, GetUserBudgetRequests, ResolveBudgetRequest, SendFunds
+from.views import ( GetUserBudgetRequests, GetProjectTransaction, GetMemberTransaction, GetBudgetRequestById, GetTransactionById, CreateTransaction, DeleteTransaction, SendFunds, ResolveBudgetRequest, UpdateTransaction
+                   )
 
 urlpatterns = [
+    path('<uuid:project_id>', GetProjectTransaction.as_view(), name='project-transaction-list'),
+    path('<uuid:project_id>/<uuid:user_id>', GetMemberTransaction.as_view(), name='member-transaction-list'),
+    path('<uuid:pk>', GetTransactionById.as_view(), name='transaction-detail'),
+    path('create', CreateTransaction.as_view(), name='create-transaction'),
+    path('delete/<uuid:pk>', DeleteTransaction.as_view(), name='delete-transaction'),
+    path('edit/<uuid:pk>', UpdateTransaction.as_view(), name='edit-transaction'),
     path('send-funds/<uuid:project_id>', SendFunds.as_view(), name='send-funds'),
     path('budget-requests', GetUserBudgetRequests.as_view(), name='budget-request-list'),
     path('budget-requests/<uuid:pk>', GetBudgetRequestById.as_view(), name='budget-request-detail'),
-    path('create', CreateTransaction.as_view(), name='create-transaction'),
     path('budget-requests/resolve/<uuid:pk>', ResolveBudgetRequest.as_view(), name='resolve-budget-request'),
 ]
