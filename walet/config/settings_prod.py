5ba0f8f2f3a01c0a297ca70b8b785a01aaa0dc6a
NI04Code <naufalichsan114@gmail.com>
2025-05-02 16:03:40 +0700

    modify configuration for production ready

--- walet/config/settings_prod.py
+++ walet/config/settings_prod.py
@@ -5,7 +5,7 @@ DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
             "NAME": os.getenv("DB_NAME"),
-            "USER": os.getenv("DB_USER"),
+            "USER": os.getenv("DB_USERNAME"),
             "PASSWORD": os.getenv("DB_PASSWORD"),
             "HOST": os.getenv("DB_HOST"),
             "PORT": os.getenv("DB_PORT", "5432"),
