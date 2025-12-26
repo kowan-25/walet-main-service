03847db8759dd2ebbc2c386033d1c900d2159a88
NI04Code <t-naufal.ichsan@traveloka.com>
2025-12-26 11:15:06 +0700

    add compose file

--- walet/config/settings_prod.py
+++ walet/config/settings_prod.py
@@ -3,19 +3,15 @@ from .settings import *
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "main-service", "https://kelompok-80-main-service.pkpl.cs.ui.ac.id", "http://main-service.kelompok-80-ns.svc.cluster.local"]
 DEBUG = False
 DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.getenv("DB_NAME"),
-            "USER": os.getenv("DB_USERNAME"),
-            "PASSWORD": os.getenv("DB_PASSWORD"),
-            "HOST": os.getenv("DB_HOST"),
-            "PORT": os.getenv("DB_PORT", "5432"),
-            "OPTIONS": {
-                "sslmode": "require",
-                "options": os.getenv("DB_OPTIONS"),
-            },
-        }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USERNAME"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT", "5432"),
     }
+}
 
 SIMPLE_JWT = {
     "ROTATE_REFRESH_TOKENS": True,
